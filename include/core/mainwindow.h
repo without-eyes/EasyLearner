34f01011a9ac7deef446695e7a4a8c4a125f929f
without-eyes <dimka180205@gmail.com>
2025-05-18 20:00:40 +0300

    Break MainWindow constructor into smaller methods

--- include/core/mainwindow.h
+++ include/core/mainwindow.h
@@ -8,13 +8,46 @@
 
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-#include <QMainWindow>
 
+#include <QMainWindow>
+#include <QStackedWidget>
+#include "mainmenu/mainmenu.h"
+#include "mainmenu/topiccreation.h"
+#include "topic/base/topicmanagement.h"
+#include "topic/content/topiccontent.h"
+#include "topic/content/topicdefinitions.h"
+#include "topic/content/topicquestions.h"
+#include "topic/creation/definitionaddition.h"
+#include "topic/creation/questionaddition.h"
+#include "topic/study/studydefinitions.h"
+#include "topic/study/studyquestions.h"
+#include "topic/study/topicstudy.h"
 
 class MainWindow final : public QMainWindow {
 public:
     MainWindow();
 
+private:
+    QStackedWidget* stackedWidget;
+    MainMenu* mainMenu;
+    TopicCreation* topicCreation;
+    TopicManagement* topicManagement;
+    TopicContent* topicContent;
+    TopicDefinitions* topicDefinitions;
+    TopicQuestions* topicQuestions;
+    DefinitionAddition* definitionAddition;
+    QuestionAddition* questionAddition;
+    TopicStudy* topicStudy;
+    StudyDefinitions* studyDefinitions;
+    StudyQuestions* studyQuestions;
+
+    void allocateFields();
+
+    void addEveryWidgetToStack() const;
+
+    void connectSignalsToSlots();
+
+    void configureMainWindow();
 };
 
 
--- src/core/mainwindow.cpp
+++ src/core/mainwindow.cpp
@@ -5,60 +5,56 @@
     * This file contains methods' implementation of MainWindow class.
 */
 
-#include "../../include/core/mainwindow.h"
-
-#include <QStackedWidget>
-#include "mainmenu/mainmenu.h"
-#include "mainmenu/topiccreation.h"
-#include "topic/base/topicmanagement.h"
-#include "topic/content/topiccontent.h"
-#include "topic/content/topicdefinitions.h"
-#include "topic/content/topicquestions.h"
-#include "topic/creation/definitionaddition.h"
-#include "topic/creation/questionaddition.h"
-#include "topic/study/studydefinitions.h"
-#include "topic/study/studyquestions.h"
-#include "topic/study/topicstudy.h"
+#include "core/mainwindow.h"
 
 MainWindow::MainWindow() {
-    this->setAttribute(Qt::WA_DeleteOnClose);
+    allocateFields();
+    addEveryWidgetToStack();
+    connectSignalsToSlots();
+    configureMainWindow();
+}
 
-    const auto stack = new QStackedWidget(this);
-    const auto mainMenu = new MainMenu(this);
-    const auto topicCreation = new TopicCreation(this);
-    const auto topicManagement = new TopicManagement(this);
-    const auto topicContent = new TopicContent(this);
-    const auto topicDefinitions = new TopicDefinitions(this);
-    const auto topicQuestions = new TopicQuestions(this);
-    const auto definitionAddition = new DefinitionAddition(this);
-    const auto questionAddition = new QuestionAddition(this);
-    const auto topicStudy = new TopicStudy(this);
-    const auto studyDefinitions = new StudyDefinitions(this);
-    const auto studyQuestions = new StudyQuestions(this);
+void MainWindow::allocateFields() {
+    stackedWidget = new QStackedWidget(this);
+    mainMenu = new MainMenu(this);
+    topicCreation = new TopicCreation(this);
+    topicManagement = new TopicManagement(this);
+    topicContent = new TopicContent(this);
+    topicDefinitions = new TopicDefinitions(this);
+    topicQuestions = new TopicQuestions(this);
+    definitionAddition = new DefinitionAddition(this);
+    questionAddition = new QuestionAddition(this);
+    topicStudy = new TopicStudy(this);
+    studyDefinitions = new StudyDefinitions(this);
+    studyQuestions = new StudyQuestions(this);
+}
 
-    stack->addWidget(mainMenu);             // 0
-    stack->addWidget(topicCreation);        // 1
-    stack->addWidget(topicManagement);      // 2
-    stack->addWidget(topicContent);         // 3
-    stack->addWidget(topicDefinitions);     // 4
-    stack->addWidget(topicQuestions);       // 5
-    stack->addWidget(definitionAddition);   // 6
-    stack->addWidget(questionAddition);     // 7
-    stack->addWidget(topicStudy);           // 8
-    stack->addWidget(studyDefinitions);     // 9
-    stack->addWidget(studyQuestions);       // 10
+void MainWindow::addEveryWidgetToStack() const {
+    stackedWidget->addWidget(mainMenu);
+    stackedWidget->addWidget(topicCreation);
+    stackedWidget->addWidget(topicManagement);
+    stackedWidget->addWidget(topicContent);
+    stackedWidget->addWidget(topicDefinitions);
+    stackedWidget->addWidget(topicQuestions);
+    stackedWidget->addWidget(definitionAddition);
+    stackedWidget->addWidget(questionAddition);
+    stackedWidget->addWidget(topicStudy);
+    stackedWidget->addWidget(studyDefinitions);
+    stackedWidget->addWidget(studyQuestions);
+}
 
-    connect(mainMenu, &MainMenu::requestPageChange, stack, &QStackedWidget::setCurrentIndex);
-    connect(topicCreation, &TopicCreation::requestPageChange, stack, &QStackedWidget::setCurrentIndex);
-    connect(topicManagement, &TopicManagement::requestPageChange, stack, &QStackedWidget::setCurrentIndex);
-    connect(topicContent, &TopicContent::requestPageChange, stack, &QStackedWidget::setCurrentIndex);
-    connect(topicDefinitions, &TopicDefinitions::requestPageChange, stack, &QStackedWidget::setCurrentIndex);
-    connect(topicQuestions, &TopicQuestions::requestPageChange, stack, &QStackedWidget::setCurrentIndex);
-    connect(definitionAddition, &DefinitionAddition::requestPageChange, stack, &QStackedWidget::setCurrentIndex);
-    connect(questionAddition, &QuestionAddition::requestPageChange, stack, &QStackedWidget::setCurrentIndex);
-    connect(topicStudy, &TopicStudy::requestPageChange, stack, &QStackedWidget::setCurrentIndex);
-    connect(studyDefinitions, &StudyDefinitions::requestPageChange, stack, &QStackedWidget::setCurrentIndex);
-    connect(studyQuestions, &StudyQuestions::requestPageChange, stack, &QStackedWidget::setCurrentIndex);
+void MainWindow::connectSignalsToSlots() {
+    connect(mainMenu, &MainMenu::requestPageChange, stackedWidget, &QStackedWidget::setCurrentIndex);
+    connect(topicCreation, &TopicCreation::requestPageChange, stackedWidget, &QStackedWidget::setCurrentIndex);
+    connect(topicManagement, &TopicManagement::requestPageChange, stackedWidget, &QStackedWidget::setCurrentIndex);
+    connect(topicContent, &TopicContent::requestPageChange, stackedWidget, &QStackedWidget::setCurrentIndex);
+    connect(topicDefinitions, &TopicDefinitions::requestPageChange, stackedWidget, &QStackedWidget::setCurrentIndex);
+    connect(topicQuestions, &TopicQuestions::requestPageChange, stackedWidget, &QStackedWidget::setCurrentIndex);
+    connect(definitionAddition, &DefinitionAddition::requestPageChange, stackedWidget, &QStackedWidget::setCurrentIndex);
+    connect(questionAddition, &QuestionAddition::requestPageChange, stackedWidget, &QStackedWidget::setCurrentIndex);
+    connect(topicStudy, &TopicStudy::requestPageChange, stackedWidget, &QStackedWidget::setCurrentIndex);
+    connect(studyDefinitions, &StudyDefinitions::requestPageChange, stackedWidget, &QStackedWidget::setCurrentIndex);
+    connect(studyQuestions, &StudyQuestions::requestPageChange, stackedWidget, &QStackedWidget::setCurrentIndex);
 
     connect(topicCreation, &TopicCreation::topicCreated, mainMenu, &MainMenu::addTopicIntoList);
     connect(topicCreation, &TopicCreation::topicCreated, mainMenu, &MainMenu::changeButtonState);
@@ -75,7 +71,10 @@ MainWindow::MainWindow() {
     connect(questionAddition, &QuestionAddition::updateStudyQuestionsButton, topicStudy, &TopicStudy::updateStudyQuestionsButton);
     connect(topicQuestions, &TopicQuestions::updateStudyButton, topicStudy, &TopicStudy::updateStudyQuestionsButton);
     connect(topicStudy, &TopicStudy::startQuestionsStudy, studyQuestions, &StudyQuestions::studyQuestion);
+}
 
-    this->setCentralWidget(stack);
+void MainWindow::configureMainWindow() {
+    this->setAttribute(Qt::WA_DeleteOnClose);
+    this->setCentralWidget(stackedWidget);
     this->resize(400, 300);
 }
